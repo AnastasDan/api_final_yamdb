ee83b241c0f748cfa8db27b799ba079d8c67d0ef
AnastasDan <danielyan.anastas@gmail.com>
2023-08-08 10:23:41 +0200

    Сделал намётки по созданию кастомного моделя User

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('user', 'Пользователь'),
+        ('moderator', 'Модератор'),
+        ('admin', 'Администратор'),
+    ]
+
+    role = models.CharField('Роль', max_length=15, choices=ROLE_CHOICES, default='user',)
+    bio = models.TextField('Биография', blank=True)
